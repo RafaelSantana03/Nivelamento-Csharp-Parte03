010458984d928c77a6d289be4cbd00a8ebe6c534
RafaelSantana03 <rafa03santana@gmail.com>
2023-06-04 17:19:17 -0300

    Estrutura While

---
+++ Atividade3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace At03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Alcool = 0;
+            int Gasolina = 0;
+            int Diesel = 0;
+
+            int tipo = int.Parse(Console.ReadLine());
+
+            while (tipo != 4) {
+                if (tipo == 1)
+                {
+                    Alcool++;
+                }
+                else if (tipo == 2)
+                {
+                    Gasolina++;
+                }
+                else if (tipo == 3)
+                {
+                    Diesel++;
+                }
+
+                tipo = int.Parse (Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine($"Alcool: {Alcool}");
+            Console.WriteLine($"Gasolina: {Gasolina}");
+            Console.WriteLine($"Diesel: {Diesel}");
+        }
+    }
+}
